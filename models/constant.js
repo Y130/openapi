c1e2da0db3c3de53d441da795ca19c98148983cc
monsterwater <github@qq.com>
2015-08-14 16:32:00 +0800

    add models

---
+++ models/constant.js
@@ -0,0 +1,38 @@
+var constant = {
+  'DELIMITER' : ',',
+  'SIGN_TYPE' : 'sign_type',
+  'SIGN_TYPE_RSA' : 'RSA',
+  'SIGN_ALGORITHMS' : 'SHA1WithRSA',
+  'FORMAT' : 'format',
+  'METHOD' : 'method',
+  'TIMESTAMP' : 'timestamp',
+  'VERSION' : 'version',
+  'SIGN' : 'sign',
+  'ACCESS_TOKEN' : 'auth_token',
+  'DATE_TIME_FORMAT' : 'yyyy-MM-dd HH:mm:ss',
+  'DATE_TIMEZONE' : 'GMT+8',
+  'CHARSET' : 'charset',
+  'CHARSET_UTF8' : 'UTF-8',
+  'CHARSET_GBK' : 'GBK',
+  'FORMAT_JSON' : 'json',
+  'FORMAT_XML'  : 'xml',
+  'CACHE_PREFIX' : 'openapi-',
+  'CACHE_PREFIX_API' : 'openapi-api-',
+  'CACHE_PREFIX_MERCHANT' : 'openapi-merchant-',
+  'CACHE_PREFIX_MERCHANTAPICONFIG' : 'openapi-merchantapiconfig-',
+  'CACHE_PREFIX_ROUTE'  : 'openapi-route-',
+  
+  'API_CACHE_KEY_FORMAT':'openapi-api-{0}-{1}',
+  'MERCHANT_CACHE_KEY_FORMAT':'openapi-merchant-{0}',
+  'MERCHANTAPICONFIG_CACHE_KEY_FORMAT':'openapi-merchantapiconfig-{0}-{1}',
+  
+  'MERCHANT' : 'merchantid',
+  'BIZ_COTENT'  : 'biz_content',
+  'FORM_UPLOAD_FILE' : 'uploadedFile',
+  'IMAGE_MEDIA_TYPE' : 'image',
+  'TEXT_MEDIA_TYPE' : 'text',
+  'CACHE_DOMAIN'  : '*',
+  'CACHE_TIMEOUT' : 86400
+}
+
+module.exports = constant;
